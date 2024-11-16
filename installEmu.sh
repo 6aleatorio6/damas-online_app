68169d0d3dee1d5c697902d6e5fc084b0a57c655
leonardo <leonardolfelix12@gmail.com>
2024-11-16 19:17:14 -0300

    chore(setup): atualizar script de instalação do emulador para compatibilidade com Fedora

--- installEmu.sh
+++ installEmu.sh
@@ -7,38 +7,63 @@
 API_LEVEL=33
 DEVICE_ID=25 #Pixel 5
 ANDROID_HOME="${HOME}/Android/Sdk"
-JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
 
 
-sudo apt-get update
-sudo apt-get install -y openjdk-17-jdk 
+if [ -f /etc/debian_version ]; then
+    PACKAGE_MANAGER="apt-get"
 
-check_and_set_variable() {
+    JAVA_PACKAGE="openjdk-17-jdk"
+    JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
+elif [ -f /etc/redhat-release ]; then
+    PACKAGE_MANAGER="dnf"
+
+    JAVA_PACKAGE="java-17-openjdk-devel"
+    JAVA_HOME="/usr/lib/jvm/jre-17-openjdk"
+else
+    echo "Sistema não suportado!"
+    exit 1
+fi
+
+sudo $PACKAGE_MANAGER update
+sudo $PACKAGE_MANAGER install -y $JAVA_PACKAGE
+
+check_and_set_variable(){
     local var_name=$1
     local var_value=$2
-    local bashrc_file="$HOME/.bashrc"
+    
+    # Lista de arquivos de shell para verificar
+    local shellrc_files=("$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.profile")  # Adicione mais arquivos, se necessário
 
-    if ! grep -q $var_name -- $bashrc_file; then
-        echo "export $var_name=\"$var_value\"" >> "$bashrc_file"
-    else
-        sed -i "s|^export $var_name=.*|export $var_name=\"$var_value\"|" "$bashrc_file"
-    fi
+    # Itera sobre os arquivos de shell
+    for shellrc_file in "${shellrc_files[@]}"; do
+        # Verifica se o arquivo existe
+        if [ -f "$shellrc_file" ]; then
+            # Adiciona ou atualiza a variável de ambiente no arquivo de configuração do shell
+            if ! grep -q "^export $var_name=" "$shellrc_file"; then
+                echo "export $var_name=\"$var_value\"" >> "$shellrc_file"
+            else
+                sed -i "s|^export $var_name=.*|export $var_name=\"$var_value\"|" "$shellrc_file"
+            fi
+        fi
+    done
 
     export "$var_name"="$var_value"
+
 }
+
 
 check_and_set_variable "ANDROID_HOME" $ANDROID_HOME
 check_and_set_variable "JAVA_HOME" $JAVA_HOME
 check_and_set_variable "PATH" "$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
 
 # ANDROID SDK
-local CMDLINE="${ANDROID_HOME}/cmdline-tools" 
-local TOOLS_ZIP_TEMP="${ANDROID_HOME}/tools.zip" 
+PATH_CMDLINE="${ANDROID_HOME}/cmdline-tools" 
+PATH_TOOLS_ZIP="${ANDROID_HOME}/tools.zip" 
 
 mkdir -p $PATH_CMDLINE
 wget https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip  -O $PATH_TOOLS_ZIP
 unzip -q $PATH_TOOLS_ZIP -d $PATH_CMDLINE  
-mv "${$PATH_CMDLINE}/cmdline-tools" "${PATH_CMDLINE}/latest" 
+mv "${PATH_CMDLINE}/cmdline-tools" "${PATH_CMDLINE}/latest" 
 rm $PATH_TOOLS_ZIP
 
 
@@ -54,7 +79,7 @@ echo no | avdmanager create avd -n AndroidPaia -k "system-images;android-${API_L
 # Configurando o teclado
 CONFIG_FILE="$HOME/.android/avd/AndroidPaia.avd/config.ini"
 if grep -q "hw.keyboard" "$CONFIG_FILE"; then
-    sed -i  's/^hw.keyboard=.*/hw.keyboard=yes/' "$CONFIG_FILE"
+    sed -i 's/^hw.keyboard=.*/hw.keyboard=yes/' "$CONFIG_FILE"
 else
     echo "hw.keyboard=yes" >> "$CONFIG_FILE"
 fi
